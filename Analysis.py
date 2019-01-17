6f9f352fa0ef1147f6466dd6282f5419f8eebffd
Samuel Guzman <[chinosamg@gmail.com]>
2019-01-16 18:29:27 -0700

    analysis

---
+++ Analysis.py
@@ -0,0 +1,8 @@
+# Analysis
+#   1)  Latitude have an inverse relationship.
+#       as max temperature increases latitude decreases.
+#   2)  Latitude  has no relationship with humidity as both vary.
+#       No relationship identified since change in latitude has
+#       low and high humidity percentages.
+#   3)  Similar to humidity and latitude cloudiness has no relationship
+#       since values vary. 
